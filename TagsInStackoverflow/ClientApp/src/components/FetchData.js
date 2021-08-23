b5f0339294d98cf628836d4b4e04de246ff991e2
Radkos76 <radkos76@gmail.com>
2021-08-23 09:35:31 +0200

    mod

--- TagsInStackoverflow/ClientApp/src/components/FetchData.js
+++ TagsInStackoverflow/ClientApp/src/components/FetchData.js
@@ -43,7 +43,7 @@ export class FetchData extends Component {
     return (
       <div>
         <h1 id="tabelLabel" >Popularity Tags in StackOverFlow</h1>
-        <p>List First 10000 Tags sorted by popularity</p>
+        <p>List Tags sorted by popularity</p>
         {contents}
       </div>
     );
